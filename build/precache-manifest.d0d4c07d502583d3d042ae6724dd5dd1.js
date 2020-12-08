a5b8cf06803f6ab5408231f4dc6210a02ed71d56
unknown <nathan.c.ginn@gmail.com>
2020-12-08 15:11:12 -0800

    dhfgakj

---
+++ build/precache-manifest.d0d4c07d502583d3d042ae6724dd5dd1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d714116556bbb0c954dc77b7f8aa2e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c52d99d848dd33b264e7",
+    "url": "/static/css/main.3a022bdf.chunk.css"
+  },
+  {
+    "revision": "c9d1f22cf7625c9c1da6",
+    "url": "/static/js/2.8a670a5e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.8a670a5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c52d99d848dd33b264e7",
+    "url": "/static/js/main.f07645f1.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "93d9f9cd8f00caa9d82a1d0f0e1798b6",
+    "url": "/static/media/mosh.93d9f9cd.png"
+  },
+  {
+    "revision": "175dbd0e2a3329111aa7362f52488383",
+    "url": "/static/media/trade-jack.175dbd0e.png"
+  },
+  {
+    "revision": "f3275bbfe29a00e67986d2c43064a98a",
+    "url": "/static/media/vivere.f3275bbf.png"
+  },
+  {
+    "revision": "f13f6b34fe50d70be48743c7e2dea094",
+    "url": "/static/media/weather-app.f13f6b34.png"
+  }
+]);
